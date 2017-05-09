fc89bf2310686e72cd26e6c914ca6ce104bebbcc
Eztharia <eztharia35@gmail.com>
2017-05-09 22:58:07 +0200

    Remove dir

--- modules/langswitcher/Info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    return [
-        'name'          =>  $core->lang['langswitcher']['module_name'],
-        'description'   =>  $core->lang['langswitcher']['module_desc'],
-        'author'        =>  'Sruu.pl',
-        'version'       =>  '1.1',
-        'icon'          =>  'flag',
-    ];
